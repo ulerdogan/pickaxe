358e408c0633a3d11031087f718d2d023259b4ec
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2023-03-07 22:48:05 +0300

    config utils - env

---
+++ utils/config.go
@@ -0,0 +1,31 @@
+package config_utils
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBUsername    string `mapstructure:"DB_USERNAME"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	DBName        string `mapstructure:"DB_NAME"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	RPCAddress    string `mapstructure:"RPC_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
