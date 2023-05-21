d9e60f4a1be072b0d5bfcdd890168131ed31ebdb
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2023-05-21 02:39:12 +0200

    use pending status

--- clients/starknet/client.go
+++ clients/starknet/client.go
@@ -23,7 +23,7 @@ func NewStarknetClient(cnfg config.Config) Client {
 }
 
 func (c *starknetClient) Call(fc types.FunctionCall) ([]string, error) {
-	return c.Rpc.Call(context.Background(), fc, rpc.WithBlockTag("latest"))
+	return c.Rpc.Call(context.Background(), fc, rpc.WithBlockTag("pending"))
 }
 
 func (c *starknetClient) GetEvents(from, to uint64, address string, c_token *string, keys []string) ([]rpc.EmittedEvent, *string, error) {
